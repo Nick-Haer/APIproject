7d49e48e3e12d11a71a72ab67b6b07cfacf582bb
Nick.Haer <nhaer123@gmail.com>
2019-07-16 13:13:53 -0500

    added some styling, and fixed gif display to work properly

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-console.log("sup")
+
+let buttonDisplay = $("#buttonsgohere")
+let gifsDisplay = $("#gifsgohere")
+
+
+let nature = ["tree", "mountain", "fish", "stream", "hill", "path", "forest", "volcano", "lake", "river"];
+
+function makeButtons() {
+    for (let element of nature) {
+       let madeButton =  $(`<button class = "clickit" data-name = "${element}">${element}</button>`)
+       madeButton.prependTo(buttonDisplay)
+    }
+}
+
+
+function summonGifs () {
+
+    gifsDisplay.empty()
+
+    let queried = $(this).attr("data-name")
+
+    let queryUrl = `https://api.giphy.com/v1/gifs/search?api_key=oKCHtfIbY0Dr7JB925gVyaUoyUIT0PzE&q=${queried}&limit=10&offset=0&rating=G&lang=en`
+
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response){
+        for (i=0; i <nature.length; i++) {
+            console.log(response)
+            let nextSrc = response.data[i].images.fixed_width.url
+            let nextGif = $(`<img>`)
+            nextGif.attr("src", nextSrc)
+            let nextP = $("<p>")
+            nextP.text(response.data[i].rating)
+            nextP.appendTo(gifsDisplay)
+            nextGif.appendTo(gifsDisplay)
+        }
+    })
+}
+
+
+
+$(document).on("click", ".clickit", summonGifs)
+
+
+
+
+
+makeButtons()
